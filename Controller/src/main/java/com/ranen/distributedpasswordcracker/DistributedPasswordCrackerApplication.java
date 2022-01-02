df32381092684a1c263ded27efc5d2fe56b4f34b
Ranen Liu <ranenliu@microsoft.com>
2022-01-01 18:56:22 -0800

    Got basic application working

--- Controller/src/main/java/com/ranen/distributedpasswordcracker/DistributedPasswordCrackerApplication.java
+++ Controller/src/main/java/com/ranen/distributedpasswordcracker/DistributedPasswordCrackerApplication.java
@@ -2,12 +2,21 @@ package com.ranen.distributedpasswordcracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class DistributedPasswordCrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedPasswordCrackerApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String sayHello() {
+		return String.format("Hi Ranen");
 	}
 
 }
