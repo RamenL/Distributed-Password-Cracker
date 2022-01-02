40c099d781231f61427e92127be7b1af914eafa7
Ranen Liu <ranenliu@microsoft.com>
2022-01-01 16:54:45 -0800

    Add files generated by spring initializer

---
+++ src/main/java/com/ranen/distributedpasswordcracker/DistributedPasswordCrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ranen.distributedpasswordcracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedPasswordCrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedPasswordCrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranen/distributedpasswordcracker/DistributedPasswordCrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranen.distributedpasswordcracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedPasswordCrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
